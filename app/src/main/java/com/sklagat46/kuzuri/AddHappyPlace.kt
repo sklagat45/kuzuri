c8cb4af87cccb05fc3b790c643f0cbcd16f2364d
sklagat45 <sklagat46@gmail.com>
2020-12-15 09:10:54 +0300

    feature-addHappy-Place

---
+++ app/src/main/java/com/sklagat46/kuzuri/AddHappyPlace.kt
@@ -0,0 +1,11 @@
+package com.sklagat46.kuzuri
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddHappyPlace : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_happy_place)
+    }
+}
