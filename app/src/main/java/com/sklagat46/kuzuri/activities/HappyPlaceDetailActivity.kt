9c30a7fa04cb4bc05263ab0476edb1cfcb659040
sklagat45 <sklagat46@gmail.com>
2020-12-23 02:26:17 +0300

    feature-addHappy-Place_details

--- app/src/main/java/com/sklagat46/kuzuri/activities/HappyPlaceDetailActivity.kt
+++ app/src/main/java/com/sklagat46/kuzuri/activities/HappyPlaceDetailActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.sklagat46.kuzuri.models.HappyPlaceModel
 import com.sklagat46.kuzuri.R
 
 class HappyPlaceDetailActivity : AppCompatActivity() {
